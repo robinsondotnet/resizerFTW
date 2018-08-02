15dacbeab6da5f6c93fd9a5d4b4e5281f4d670f2
Robinson Kent Villegas Rojas <rkvillegas@belatrixsf.com>
2018-08-02 16:56:00 -0500

    Add project files.

---
+++ ResizerCFW/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using TinifyAPI;
+
+namespace ResizerCFW
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            Tinify.Key = "A5lM3cfqk5BhU0nGFa0JEjKEQd550JAO";
+
+            const string outPath = "C://Users//rkvillegas//output";
+
+            var directories =
+                Directory.GetDirectories(
+                    "C://Users//rkvillegas//Downloads//Pictures-20180802T153825Z-001//Pictures");
+
+            foreach (var dir in directories)
+            {
+                foreach (var file in Directory.GetFiles(dir))
+                {
+                    var fileInfo = new FileInfo(file);
+                    var src = await Tinify.FromFile(file);
+                    src = src
+                        .Resize(new
+                        {
+                            method = "fit",
+                            width = 2408,
+                            height = 1365,
+                        });
+
+                        await src.ToFile($"{outPath}//{fileInfo.Name}");
+                }
+            }
+
+            Console.ReadLine();
+
+            return 0;
+        }
+    }
+}
---
+++ ResizerFTW/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+open TinifyAPI
+
+[<EntryPoint>]
+let main argv =
+    Tinify.Key = ""
+
+    let files = Directory.GetFiles "C://Users//rkvillegas//Downloads//Pictures-20180802T153825Z-001//Pictures//Lima"
+    files |> Array.iter (fun t -> async{  Tinify.FromFile t |> Async.AwaitTask })
+
+    printfn "Hello World from F#!"
+    Console.ReadLine() |> ignore
+    0
